c55ba08d557a7d3a77b41d1da2dc08c030ad0dcd
kanitsharma <kanitshrm09@gmail.com>
2018-06-27 19:48:19 +0530

    added map, chain, fold, of, ap, curry to combinators

---
+++ packages/combinators/src/ap.js
@@ -0,0 +1,3 @@
+import { curry } from 'ramda'
+
+export default curry((a1, a2) => a1.ap(a2))
--- packages/combinators/src/index.js
+++ packages/combinators/src/index.js
@@ -1,5 +1,8 @@
 import K from './k'
 import I from './i'
 import compose from './compose'
+import map from './map'
+import chain from './chain'
+import curry from './curry'
 
-export { I, K, compose }
+export { I, K, compose, map, chain, curry }
