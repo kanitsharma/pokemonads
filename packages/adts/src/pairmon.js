b84897d2be6e60650706169e3ff6791381061490
kanitsharma <kanitshrm09@gmail.com>
2018-07-17 12:12:23 +0530

    Implemented Functor, Monad instance

--- packages/adts/src/pairmon.js
+++ packages/adts/src/pairmon.js
@@ -1,3 +1,5 @@
+// TODO: implement functor. monad, monoid
+
 import { tagged } from 'daggy'
 
 const Pair = tagged('Pair', ['value', 'state'])
@@ -9,5 +11,19 @@ Pair.prototype.fst = function() {
 Pair.prototype.snd = function() {
   return this.state
 }
+
+// Funtor :: Fa -> (a -> b) -> Fb
+
+Pair.prototype.map = function(fn) {
+  return Pair(this.value, fn(this.state))
+}
+
+// Monad :: Ma -> (a -> Mb) -> Mb
+
+Pair.prototype.chain = function(fn) {
+  return fn(this)
+}
+
+// Bimap :: Fab -> (a -> c) -> (b -> d) -> Fcd
 
 export default Pair
