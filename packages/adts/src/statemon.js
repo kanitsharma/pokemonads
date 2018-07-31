4d29ebe2980d12ea6c94d2c137dfd2efb17dbf30
kanitsharma <kanitshrm09@gmail.com>
2018-07-31 21:55:29 +0530

    Added type sig to statemon

--- packages/adts/src/statemon.js
+++ packages/adts/src/statemon.js
@@ -5,18 +5,21 @@ import Pair from './pairmon'
 const State = st => ({
   // runState :: a -> State
   runState: x => st(x),
+
   // Functor map :: (a -> b) -> fa -> fb
   map: f =>
     State(x => {
       const { value, state } = st(x)
       return Pair(f(value), state)
     }),
+
   // Monad chain :: (a -> mb) -> ma -> mb
   chain: f =>
     State(s => {
       const { value, state } = st(s)
       return f(value).runState(state)
     }),
+
   // Applicative ap :: ma -> mb -> mab
   ap: state =>
     State(s => {
@@ -24,26 +27,31 @@ const State = st => ({
       const { value2, state2 } = state.runState(state)
       return Pair(value(value2), state2)
     }),
+
   // get :: () -> State
   get: () =>
     State(s => {
       const { state } = st(s)
       return Pair(state, state)
     }),
+
   // modify :: (a -> b) -> State
   modify: f => State(s => Pair(undefined, f(s))),
+
   // put :: State -> State
   put: s =>
     State(ss => {
       const { value } = st(ss)
       return Pair(value, s)
     }),
+
   // push :: () -> State
   push: () =>
     State(ss => {
       const { value } = st(ss)
       return Pair(value, value)
     }),
+
   // then :: (a -> b) -> State
   then: f =>
     State(s => {
@@ -51,6 +59,7 @@ const State = st => ({
       const res = f(value)
       return res.runState ? res.runState(state) : Pair(res, state)
     }),
+
   withState: f => State(s => Pair(f(...st(s)), s)),
   exec: x => st(x).state,
   eval: x => st(x).value,
